fc009a0204dd736d6f3c1c49c0e7dbedabfd581b
nkartikaxioned <kartikn@axioned.com>
2023-10-09 16:22:39 +0530

    js functionality add

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,29 @@
-/* Author: 
-
-*/
-
-
+const cities = document.querySelectorAll('.cities'), 
+city = document.querySelectorAll('.city'), 
+link = document.querySelectorAll('a');
 
 
+link.forEach((element,index) => {
+    element.addEventListener('click', () => {
+       removeActive();
+       removeActiveCity();
+       element.classList.add('active');
+       city[index].classList.add('active-city');
+    });
+});
+
+ let removeActive = () => {
+    link.forEach((elem) => {
+       elem.classList.remove('active');
+    })
+ }
 
 
+ let removeActiveCity = () => {
+    city.forEach((elem) => {
+        elem.classList.remove('active-city')
+    })
+ }
 
 
 
